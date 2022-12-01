05185136351156603a5a81a0a7bdd65ec42db2aa
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2022-11-30 21:20:23 -0300

    fix: renaming the project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "template_base_production"
+  # config.active_job.queue_name_prefix = "kode_knight_production"
 
   config.action_mailer.perform_caching = false
 
