ac73d351770fb1d7a1b99e33471c751604845077
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2022-12-06 19:07:45 -0300

    feat: adding auth to admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   root 'application#index'
 
-  scope 'admin' do
-    get '/index', to: 'application#admin_index', as: 'admin_index'
-    resources :courses, except: [:show]
-    resources :projects
+  scope '/pessoa_humana' do
+    devise_for :admins, controllers: {
+      registrations: 'admins/registrations'
+    }
+  end
+
+  authenticate :admin do
+    scope '/ugly' do
+      get '/index', to: 'application#admin_index', as: 'admin_index'
+      resources :courses, except: [:show]
+      resources :projects
+    end
   end
 end
