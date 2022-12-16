d379453a7ac478a99792155a58041d581ff72c23
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2022-12-16 17:51:52 -0300

    style: translating course dates

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,9 @@
 module CoursesHelper
+  def translate_date(date)
+    month, year = date.split('/')
+
+    month = I18n.t "date.month.#{month.downcase}"
+
+    "#{month.capitalize}/#{year}"
+  end
 end
